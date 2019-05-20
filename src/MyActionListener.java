297d5ac4c5b868a6699f3a89d85a56d99b1c8364
Arie <arie.lubashiz@gmail.com>
2019-05-20 18:39:21 +0300

    Update.

--- src/MyActionListener.java
+++ src/MyActionListener.java
@@ -2,53 +2,39 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JRadioButton;
 
-/*
- * Created on Tevet 5770 
- */
-
 /**
- * @author לויאן
+ * It represent the action listener for the traffic system.
+ * @author Arie and Gad.
  */
-
-
 public class MyActionListener implements ActionListener
 {
-	Event64 evButtonPressed;
-	Event64 evShabatButtonPressed;
-	Event64 evShabtButtonUnPressed;
+	private Event64 evButtonPressed;
+	private Event64 evShabatButtonPressed;
+	private Event64 evShabatButtonReleased;
 
-	public MyActionListener(){
-		 evButtonPressed = new Event64();
-		 evShabatButtonPressed = new Event64();
-		 evShabtButtonUnPressed = new Event64();
-
+	public MyActionListener() {
+		this.evButtonPressed = new Event64();
+		this.evShabatButtonPressed = new Event64();
+		this.evShabatButtonReleased = new Event64();
 	}
 
-	public void init(Event64 evButtonPressed_,Event64 evShabatButtonPressed_,Event64 evShabtButtonUnPressed_){
-		evButtonPressed = evButtonPressed_;
-		evShabatButtonPressed = evShabatButtonPressed_;
-		evShabtButtonUnPressed = evShabtButtonUnPressed_;
-
+	public void init(Event64 evButtonPressed, Event64 evShabatButtonPressed, Event64 evShabatButtonReleased) {
+		this.evButtonPressed = evButtonPressed;
+		this.evShabatButtonPressed = evShabatButtonPressed;
+		this.evShabatButtonReleased = evShabatButtonReleased;
 	}
 
-	public void actionPerformed(ActionEvent e) 
-	{
-		JRadioButton butt=(JRadioButton)e.getSource();
+	public void actionPerformed(ActionEvent e) {
+		JRadioButton butt = (JRadioButton)e.getSource();
 		int key = Integer.parseInt(butt.getName());
 
-		if(key == 16){
+		if (key == 16) {
 			if (butt.isSelected())
 				evShabatButtonPressed.sendEvent();
-			 else
-				evShabtButtonUnPressed.sendEvent();
+			else
+				evShabatButtonReleased.sendEvent();
 		}
 		else
 			evButtonPressed.sendEvent(key);
-
-
-
-
-
 	}
-
 }
